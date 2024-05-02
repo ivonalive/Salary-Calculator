b0e255058a1082548674cfa952018fbefd7549e2
Ivona Olvera-Radenovic <ivonaolvera@gmail.com>
2024-05-02 15:55:37 -0700

    form to body info
    Co-authored-by: Sam Freeman <sam-c-freeman@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,35 @@
+// Checking if the js file is connected
+console.log('js');
+
+function addEmployee(event){
+    // Checking if the button is wokring
+    console.log('Does this button/function work?');
+    event.preventDefault();
+
+    // Connecting each inout field to table
+    let firstName = document.querySelector('[data-testid="firstNameInput"]').value;
+    let lastName = document.querySelector('[data-testid="lastNameInput"]').value;
+    let idInput = document.querySelector('[data-testid="lastNameInput"]').value;
+    let title = document.querySelector('[data-testid="titleInput"]').value;
+    let salary = document.querySelector('[data-testid="annualSalaryInput"]').value;
+    // Checking if id refrences are working properly
+    console.log(firstName);
+
+    // Getting id of table body to append to
+    let tBody = document.querySelector('#tbody');
+    tBody.innerHTML += `
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idInput}</td>
+        <td>${title}</td>
+        <td>${salary}</td>
+        <td> <button onCLick="deleteEmployee(event)"> Delete </button></td>
+     </tr>
+
+    `;
+};
+
+function deleteEmployee(event){
+    event.target.parentElement.parentElement.remove();
+}
